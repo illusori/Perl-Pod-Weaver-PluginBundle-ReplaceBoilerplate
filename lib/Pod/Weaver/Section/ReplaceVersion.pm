59d594c77ee76a9d67c2c54069e6f8775dcfede1
Sam Graham <git@illusori.co.uk>
2011-05-29 14:00:12 +0100

    Updated copyright year.

--- lib/Pod/Weaver/Section/ReplaceVersion.pm
+++ lib/Pod/Weaver/Section/ReplaceVersion.pm
@@ -74,7 +74,7 @@ Sam Graham <libpod-weaver-pluginbundle-replaceboilerplate-perl BLAHBLAH illusori
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Sam Graham <libpod-weaver-pluginbundle-replaceboilerplate-perl BLAHBLAH illusori.co.uk>.
+This software is copyright (c) 2010-2011 by Sam Graham <libpod-weaver-pluginbundle-replaceboilerplate-perl BLAHBLAH illusori.co.uk>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
